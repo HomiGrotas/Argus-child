63ad4c0a97eda5b2dafbdd9491d31660a39fb091
homigrotas <shaninadav10@gmail.com>
2022-03-10 19:33:55 +0200

    added interface and removed JWT token

--- src/com/company/API/ApiAuth.java
+++ src/com/company/API/ApiAuth.java
@@ -1,24 +1,13 @@
 package com.company.API;
-import kong.unirest.HttpResponse;
-import kong.unirest.Unirest;
 
 import com.company.utils.Config;
 
-
 public class ApiAuth {
-    public static String token = null;
     public static String mac_address = null;
     public static String static_token = null;
+}
 
-    /**
-     * Note that mac_address should be separated with '*' instead of ':'
-     */
-    public static void updateToken() {
-        HttpResponse<String> response = Unirest.get(
-                        Config.properties.getProperty("BASE_URL") + "/token")
-                .basicAuth(mac_address, static_token)
-                .asString();
-        token = response.getBody();
-        System.out.println(token);
-    }
+interface API
+{
+    String base_url = Config.properties.getProperty("BASE_URL");
 }
