45367330faee67cdd322e1dd89ef14624972ebf4
homigrotas <shaninadav10@gmail.com>
2022-03-10 19:36:32 +0200

    handles API interface

--- src/com/company/API/ChildAPI.java
+++ src/com/company/API/ChildAPI.java
@@ -9,7 +9,7 @@ import kong.unirest.Unirest;
 import kong.unirest.json.JSONObject;
 import unirest.shaded.com.google.gson.JsonObject;
 
-public class ChildAPI {
+public class ChildAPI implements API{
     public static final String endpoint = "/child";
 
     public static Pair<Boolean, String> createChild(String mac_address, String nickname, String registration_token)
@@ -19,13 +19,13 @@ public class ChildAPI {
         JSONObject json;
 
         JsonObject bodyParams = new JsonObject();
-        bodyParams.addProperty("mac_address", mac_address);
+        bodyParams.addProperty("mac_address", mac_address.replaceAll("\\*", ":"));
         bodyParams.addProperty("nickname", nickname);
         bodyParams.addProperty("parent_token", registration_token);
 
         try {
             HttpResponse<JsonNode> response = Unirest.post(
-                            Config.properties.getProperty("BASE_URL") + endpoint)
+                            base_url + endpoint)
                     .header("Content-Type", "application/json")
                     .body(bodyParams)
                     .asJson();
@@ -48,6 +48,7 @@ public class ChildAPI {
             created = false;
             if (json.has("message"))
             {
+                System.out.println(json);
                 message = json.getString("message");
             }
             else{
@@ -59,8 +60,5 @@ public class ChildAPI {
 
 
     public static void main(String[] args) {
-        String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NTcsImV4cCI6MTY0NTEwNTcyOS4xMjE5ODA0fQ.JA8ZQR1zYgAnxJyF6V8P0nVIOANj7UVLqDMJopoxOJs";
-        System.out.println(createChild("10:10:5e:02:23:aa", "12ksmksmx3",
-        token));
     }
 }
