44173daeeec8ea8f5c2ad5908a9c34550e3d6ad9
homigrotas <shaninadav10@gmail.com>
2022-03-20 12:09:35 +0200

    API reports to server child alive

--- src/com/company/API/AliveAPI.java
+++ src/com/company/API/AliveAPI.java
@@ -1,5 +1,22 @@
 package com.company.API;
 
-public class AliveAPI implements API {
+import kong.unirest.HttpResponse;
+import kong.unirest.JsonNode;
+import kong.unirest.Unirest;
+import kong.unirest.json.JSONObject;
 
+public class AliveAPI implements API {
+    public static final String endpoint = "/child/activity";
+
+    public static void postActive()
+    {
+        HttpResponse<JsonNode> response = null;
+
+        try {
+             Unirest.post(
+                    base_url + endpoint)
+                    .basicAuth(ApiAuth.mac_address, ApiAuth.static_token)
+                    .asJson();
+        }catch (kong.unirest.UnirestException ignored){}
+    }
 }
