8b79031811567dba15136b0d96cb731eede11824
homigrotas <shaninadav10@gmail.com>
2022-03-27 20:14:22 +0300

    internet or auth fail support

--- src/com/company/local/LocalMain.java
+++ src/com/company/local/LocalMain.java
@@ -6,6 +6,7 @@ import com.company.API.ChildAPI;
 import com.company.local.timeLimit.MonitorTime;
 import com.company.utils.Child;
 
+
 /**
  * This class is called after the client was logged
  * in successfully and started running
@@ -13,25 +14,27 @@ import com.company.utils.Child;
 public class LocalMain {
     private static final Child child = new Child(ChildAPI.getChild());
 
-    public static void run()
-    {
+    public static void run() {
         System.out.println("Loading data...");
 
         // check internet connectivity and correct running machine
-        if (child.mac_address == null ||
-                !child.mac_address.replace(":", "*").equals(ApiAuth.mac_address)){
-            throw new RuntimeException("Error: No internet connection could made to server" +
-                    " or failed to authenticate with server");
-        }
+        if (child.mac_address == null){
+            System.out.println("Error: No Internet");
+        }else
+            if (!child.mac_address.replace(":", "*").equals(ApiAuth.mac_address)){
+                System.out.println("Error: couldn't authenticate with the server." +
+                        "Can be caused by a broken or stolen child token.");
+            }else {
 
-        Websites websites = new Websites();
-        Apps apps = new Apps();
-        MonitorTime monitorTime = new MonitorTime(child);
+                Websites websites = new Websites();
+                Apps apps = new Apps();
+                MonitorTime monitorTime = new MonitorTime(child);
 
-        System.out.println("Starting all threads...");
+                System.out.println("Starting all threads...");
 
-        apps.start();
-        websites.start();
+                apps.start();
+                websites.start();
+            }
 
     }
 }
